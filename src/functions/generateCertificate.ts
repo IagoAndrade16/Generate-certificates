9801b2dc5cc69cf244772001fc8d6b3e4efc98e3
Iago Alexandre <iagoaap@outlook.com>
2023-01-23 18:54:10 -0300

    create certificate template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,22 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
+import * as dayjs from "dayjs"; 
+import { join } from "path";
+import { compile } from "handlebars"
+import { readFileSync } from "fs";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -19,6 +31,27 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       created_at: new Date().getTime()
     }
   }).promise();
+
+  const compileTemplate = async (data: ITemplate) => {
+    const filePath = join(process.cwd(), "src", "templates", "certificates.hbs")
+
+    const html = readFileSync(filePath, "utf-8");
+
+    return compile(html)(data)
+  }
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    name,
+    id, 
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal 
+  }
+
+  const content = await compileTemplate(data)
 
   const response = await document.query({
     TableName: "users_certificate",
